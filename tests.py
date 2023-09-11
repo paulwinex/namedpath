254f1f14bf0dc2a30bdb49a635b6cdd4ea8c7e39
paulwinex <paulwinex@gmail.com>
2023-09-11 13:59:20 +0300

    updated tests

--- tests.py
+++ tests.py
@@ -32,10 +32,10 @@ def patterns():
 @pytest.fixture()
 def context():
     return dict(
-        project_name='example',
-        entity_name='sh001',
-        version=15,
-        ext='exr'
+        PROJECT_NAME='example',
+        ENTITY_NAME='sh001',
+        VERSION=15,
+        EXT='exr'
     )
 
 
@@ -53,6 +53,7 @@ def path_ctl1(tree):
 def path_ctl2(tree):
     return tree.get_path_instance('ASSET_MODELS')
 
+# =======================================================
 
 def test_unique_patterns(tree, context):
     tree.check_uniqueness_of_parsing(context)
@@ -80,7 +81,7 @@ def test_parsing_context(context, path_ctl1):
 
 
 def test_tree_props(tree):
-    assert tree.path == '/tmp/my_struct'
+    assert tree.root == '/tmp/my_struct'
     assert tree.get_path_names() == ('ASSET', 'ASSETS', 'ASSET_MODELS', 'PROJECT', 'SHOT', 'SHOTS', 'SHOT_PUBLISH')
     assert tree.get_path_instance('ASSET').name == 'ASSET'
 
@@ -104,7 +105,7 @@ def test_path_glob_pattern(path_ctl1):
 
 def test_path_regex_pattern(path_ctl1):
     pat = path_ctl1.as_regex()
-    assert pat == r'^(?P<project_name>[^/\\]+)/shot/(?P<entity_name>[^/\\]+)/publish/v(?P<version>[^/\\]+)/[^/\\]+_v[^/\\]+\.(?P<ext>[^/\\]+)$'
+    assert pat == r'^(?P<PROJECT_NAME>[^/\\]+)/shot/(?P<ENTITY_NAME>[^/\\]+)/publish/v(?P<VERSION>[^/\\]+)/[^/\\]+_v[^/\\]+\.(?P<EXT>[^/\\]+)$'
 
 
 def test_path_permissions_list(path_ctl1):
@@ -144,6 +145,75 @@ def test_path_values(path_ctl1, path_ctl2):
 
 def test_options_preset(tree):
     assert tree.get_path_instance('ASSET').options == {'path': '[ASSETS]/{ENTITY_NAME}', 'user': 'test', 'perm': '755'}
+
+
+def test_context_value_type(tree, context):
+    assert tree.parse('/tmp/my_struct/example/shot/sh001/publish/v015/sh001_v015.exr', with_context=True)[1]['VERSION'] == 15
+
+
+
+@pytest.fixture()
+def path_list1():
+    return dict(
+    PROJECT='{PROJECT_NAME}',
+    CONFIG={
+        'path': '[PROJECT]/.config'
+    },
+    SHOTS={
+        'path': '[PROJECT]/shots',
+        'symlink_to': '{MNT_SHOTS}'
+    },
+    SHOT={
+        'path': '[SHOTS]/{ENTITY_NAME}/{ENTITY_NAME}{FRAME:04d}.{EXT}',
+        'defaults': {'EXT': 'exr', "FRAME": ""},
+        'prefix': {'FRAME': "-"},
+        'types': {'FRAME': 'int'}
+    },
+)
+
+@pytest.fixture()
+def path_list2():
+    return dict(
+    PRJ='{PROJECT}',
+    CONF={
+        'path': '[PRJ]/.conf'
+    },
+    SHTS={
+        'path': '[PRJ]/shots',
+    },
+    SHT={
+            'path': '[SHTS]/prod/{OBJ_NAME}{FRM:03d}.{FILETYPE}',
+            'defaults': {'FILETYPE': 'exr', "FRM": ""},
+            'prefix': {'FRM': "_"},
+            'types': {'FRM': 'int'}
+        },
+)
+
+@pytest.fixture()
+def pattern_names_map():
+    return dict(
+        PROJECT='PRJ',
+        CONFIG='CONF',
+        SHOTS='SHTS',
+        SHOT='SHT',
+    )
+
+@pytest.fixture()
+def context_map():
+    return dict(
+        PROJECT_NAME='PROJECT',
+        ENTITY_NAME='OBJ_NAME',
+        FRAME='FRM',
+        EXT='FILETYPE',
+    )
+
+
+def test_transfer_structures(path_list1, path_list2, pattern_names_map, context_map):
+    t1 = NamedPathTree('/tmp/projects1', path_list1)
+    t2 = NamedPathTree('/tmp/projects2', path_list2)
+    res = t1.transfer_to(t2, pattern_names_map, context_map)
+    expected_res = {'remapped_paths': [{'old_path': '/tmp/projects1/prj1', 'new_path': '/tmp/projects2/prj1'}, {'old_path': '/tmp/projects1/prj1/.config', 'new_path': '/tmp/projects2/prj1/.conf'}, {'old_path': '/tmp/projects1/prj1/shots', 'new_path': '/tmp/projects2/prj1/shots'}, {'old_path': '/tmp/projects1/prj1/shots/box/box0001.exr', 'new_path': '/tmp/projects2/prj1/shots/prod/box001.exr'}, {'old_path': '/tmp/projects1/prj1/shots/box/box0002.exr', 'new_path': '/tmp/projects2/prj1/shots/prod/box002.exr'}, {'old_path': '/tmp/projects1/prj1/shots/cube/cube_0001.exr', 'new_path': '/tmp/projects2/prj1/shots/prod/cube001.exr'}, {'old_path': '/tmp/projects1/prj1/shots/cube/cube_0002.exr', 'new_path': '/tmp/projects2/prj1/shots/prod/cube002.exr'}], 'skipped_paths': ['/tmp/projects1/prj1/shots/box', '/tmp/projects1/prj1/shots/cube']}
+    assert res == expected_res
 
 
 # I/O TESTS
